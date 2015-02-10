39618a52e38908203c7fffd5bd78c5eb06d92489
narendra-egnaroinc <narendrar@egnaroinc.com>
2015-02-10 18:14:18 +0530

    Changed App statement(2nd)

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,6 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+    	System.out.println("Im pulling to branch and then merge");
         return ok(index.render("Your new application is ready."));
     }
 
