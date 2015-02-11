16e02edb6af3fe5b89aa20660f7137fac0d2c715
papiat-egnaroinc <papiat@egnaroinc.com>
2015-02-11 16:15:22 +0530

    papia 1st commit

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("your new application is not working.Please check"));
+        return ok(index.render("Papia"));
     }
     public static Result login() {
 		return ok(login.render());
