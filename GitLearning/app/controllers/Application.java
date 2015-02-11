0dc490f186ebbd825c3505bc0f6431d3c56466bd
sivam-egnaroinc <sivam@egnaroinc.com>
2015-02-11 16:44:17 +0530

    adding to shiva branch: string changes in application.java

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-    	System.out.println("hello i am siva");
+    	System.out.println("hello i am siva!");
         return ok(index.render("your new application is not working.Please check"));
     }
 
