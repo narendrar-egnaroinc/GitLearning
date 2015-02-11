e1c43c38d47180a13fa9905c6e7fd7f3c8346b5d
papiat-egnaroinc <papiat@egnaroinc.com>
2015-02-11 16:42:46 +0530

    this is my 3rd commit

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("your new application is not working.Please check"));
+        return ok(index.render("I'm trying to solve merge"));
     }
     public static Result login() {
 		return ok(login.render());
