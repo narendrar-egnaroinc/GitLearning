870bd1963d933fb2658aad05058a961a2f9ffb27
papiat-egnaroinc <papiat@egnaroinc.com>
2015-02-11 16:48:20 +0530

    Tommorow is my b'day

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("I'm trying to solve merge"));
+        return ok(index.render("Tommorow is my birthday"));
     }
     public static Result login() {
 		return ok(login.render());
