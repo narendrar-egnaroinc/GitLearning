6b59d7e2af9519c6641062d07c05b97f0917f44e
Arvind Kumar <arvindk@egnaroinc.com>
2015-02-11 19:43:04 +0530

    hello ....

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -9,7 +9,7 @@ public class Application extends Controller {
 
     public static Result index() {
 
-        return ok(index.render("Welcome to the world of Draunzer & city of Nawab:Arvind Agrwal"));
+        return ok(index.render("Welcome to the world of :Arvind Agrwal"));
 
     }
     
