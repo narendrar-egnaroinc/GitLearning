b81564ab07239ef6de6a962f774fe877a6257e97
Arvind Kumar <arvindk@egnaroinc.com>
2015-02-11 19:12:23 +0530

    This is final commit of 11/02/2015 from my a/c.

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Welcome to the world of Draunzer:Arvind Agrwal"));
+        return ok(index.render("Welcome to the world of Draunzer & city of Nawab:Arvind Agrwal"));
     }
     
     public static Result login() {
