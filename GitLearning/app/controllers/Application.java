c66386c0804e6d4441b29b1ef9ca6154205c0857
Arvind Kumar <arvindk@egnaroinc.com>
2015-02-11 19:39:39 +0530

    hello everyone

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Welcome to the world of Bonjour:Arvind Agrwal"));
+        return ok(index.render("Welcome to the world of Bonjour:Arvind Agrwal hello everyone."));
     }
     
     public static Result login() {
