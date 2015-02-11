dd7e4d7a72b8c75410643c566cb630f4c9ee4658
narendra-egnaroinc <narendrar@egnaroinc.com>
2015-02-11 16:39:49 +0530

    Added Admin Controller

---
+++ GitLearning/app/controllers/AdminController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class AdminController extends Controller{
+
+	public static Result AdminLogin(){
+		
+		return ok();
+	}
+}
