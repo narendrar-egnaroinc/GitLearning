d3e084ab32de4cc24a76264d68c749258780b2d5
sivam-egnaroinc <sivam@egnaroinc.com>
2015-02-19 13:18:51 +0530

    I have write one print statement in Application.java

--- GitLearning/app/controllers/Application.java
+++ GitLearning/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-
+        System.out.println("Hi Good Afternoon every body")
         return ok(index.render("Welcome to the world of :Arvind Agrwal abc ..... :) "));
 
     }
